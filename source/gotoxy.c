2cfc5df17c62f4dd1d1285097a304542e81a15c7
Minho Park <iampmh0601@gmail.com>
2020-02-07 06:32:26 +0900

    Make Enemy and Bullet

--- source/gotoxy.c
+++ source/gotoxy.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <windows.h>
 
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+
 void gotoxy(int x, int y)
 {
     COORD CursorPosition = {x, y};
--- source/main.c
+++ source/main.c
@@ -11,47 +11,105 @@ const int refreshRate = 50; // 주파수
 int main()
 {
     setcursortype(NOCURSOR);
-    int x = 0, y = 0;
-    int cnt = 1;
+    int x = 40, y = 20, cnt = 1, n = 0;
+    int emX[5] = {0, 10, 20, 30, 40},
+        emY[5] = {-1, -50, -10, -25, -15},
+        bulX[10] = {
+            0,
+        },
+        bulY[10] = {
+            0,
+        };
     char ch;
     gotoxy(x, y);
     printf("@");
     while (true)
     {
+        // User
         ch = '\0';
         if (kbhit()) // 키보드가 입력 받을 때만
-            ch = getch();
-
-        gotoxy(x, y);
-        printf(" ");
-        switch (ch)
         {
-        case 'w':
-            if (y > 0)
-                y--;
-            break;
-        case 's':
-            if (y < 24)
-                y++;
-            break;
-        case 'a':
-            if (x > 0)
-                x--;
-            break;
-        case 'd':
-            if (x < 79)
-                x++;
+            ch = getch();
+            gotoxy(x, y);
+            printf(" ");
+
+            switch (ch)
+            {
+            case 'a':
+                if (n > 10)
+                    n = 0;
+                bulX[n] = x;
+                bulY[n] = y - 1;
+                n++;
+                break;
+            case UP:
+                if (y > 0)
+                    y--;
+                break;
+            case DOWN:
+                if (y < 24)
+                    y++;
+                break;
+            case LEFT:
+                if (x > 0)
+                    x--;
+                break;
+            case RIGHT:
+                if (x < 79)
+                    x++;
+                break;
+            }
+            gotoxy(x, y);
+            printf("@");
         }
-        gotoxy(x, y);
-        printf("@");
 
-        gotoxy(0, 0);
-        printf("%d", cnt);          // 1/50 초마다 1씩 증가
-        if (cnt % refreshRate == 0) // 1초마다 1씩 증가
-            printf("\n%d", cnt / refreshRate);
+        // Enemy
+        if (cnt % 10 == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                gotoxy(emX[i], emY[i]);
+                printf(" ");
+                if (emY[i] > 24)
+                    emY[i] = -1;
+                else
+                    emY[i]++;
+                if (emY[i] >= 0)
+                {
+                    gotoxy(emX[i], emY[i]);
+                    printf("$");
+                }
+            }
+        }
+        if (cnt % 5 == 0)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                gotoxy(bulX[i], bulY[i]);
+                printf(" ");
+                if (bulY[i] <= 0)
+                    bulY[i] = -1;
+                else
+                    bulY[i]--;
 
-        Sleep(1000 / refreshRate); // 주기 = 1/주파수
+                if (bulY[i] >= 0)
+                {
+                    gotoxy(bulX[i], bulY[i]);
+                    printf("!");
+                }
+            }
+        }
+
+        Sleep(1000 / refreshRate);
         cnt++;
+        // 시분할 Time Sharing example
+        // gotoxy(0, 0);
+        // printf("%d", cnt);          // 1/50 초마다 1씩 증가
+        // if (cnt % refreshRate == 0) // 1초마다 1씩 증가
+        //     printf("\n%d", cnt / refreshRate);
+
+        // Sleep(1000 / refreshRate); // 주기 = 1/주파수
+        // cnt++;
     }
     return 0;
 }
